aa46c3864808f75d8ce31b5bc0fdae74295cc613
James Chen <chiaching789@gmail.com>
2019-10-21 18:14:27 -0400

    Updates

---
+++ precache-manifest.743fdd347b97fb08f2f08868eaf3f42e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a9011b595a8913e00ae7b71509f7fb",
+    "url": "/player-value-calculator/index.html"
+  },
+  {
+    "revision": "b3d6016a7d7f0ddea117",
+    "url": "/player-value-calculator/static/css/main.38dd50f9.chunk.css"
+  },
+  {
+    "revision": "889aa651c940e593da1a",
+    "url": "/player-value-calculator/static/js/2.c678095d.chunk.js"
+  },
+  {
+    "revision": "b3d6016a7d7f0ddea117",
+    "url": "/player-value-calculator/static/js/main.04d80654.chunk.js"
+  },
+  {
+    "revision": "be9c2f4c22a12bdf8b93",
+    "url": "/player-value-calculator/static/js/runtime-main.63416e03.js"
+  }
+]);
